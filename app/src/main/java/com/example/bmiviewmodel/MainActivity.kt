b237597a4424c0dfdf5c3c722942e60fc31b1455
JTauri <jarno.tauriainen@gmail.com>
2024-10-17 19:07:57 +0300

    Siivoilua ja pikkumuutoksia

--- app/src/main/java/com/example/bmiviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/bmiviewmodel/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,12 +18,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bmiviewmodel.ui.theme.BMIViewModelTheme
-import com.example.bmiviewmodel.R
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.example.bmiviewmodel.ui.viewmodel.BMIViewModel
@@ -47,12 +38,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Bmi(BMIViewModel: BMIViewModel = viewModel()) {
+fun Bmi(bmiViewModel: BMIViewModel = viewModel()) {
 
-    val heightInput = BMIViewModel.heightInput
-    val weightInput = BMIViewModel.weightInput
-    val bmi = BMIViewModel.calculateBMI()
-
+    val heightInput = bmiViewModel.heightInput
+    val weightInput = bmiViewModel.weightInput
+    val bmi = bmiViewModel.calculateBMI()
 
     Column(
         modifier = Modifier.fillMaxWidth().padding(top = 16.dp, start = 2.dp, end = 2.dp)
@@ -61,12 +51,12 @@ fun Bmi(BMIViewModel: BMIViewModel = viewModel()) {
             text = stringResource(R.string.body_mass_index),
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.primary,
-            textAlign = TextAlign.End,
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp)
         )
         OutlinedTextField(
             value = heightInput,
-            onValueChange = { BMIViewModel.changeHeightInput(it.replace(',', '.')) },
+            onValueChange = { bmiViewModel.changeHeightInput(it.replace(',', '.')) },
             label = { Text(text = stringResource(R.string.height)) },
             singleLine = true,
             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
@@ -74,14 +64,14 @@ fun Bmi(BMIViewModel: BMIViewModel = viewModel()) {
         )
         OutlinedTextField(
             value = weightInput,
-            onValueChange = { BMIViewModel.changeWeightInput(it.replace(',', '.')) },
+            onValueChange = { bmiViewModel.changeWeightInput(it.replace(',', '.')) },
             label = { Text(text = stringResource(R.string.weight)) },
             singleLine = true,
             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
             modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
         )
 
-        Text(text = stringResource(R.string.result, String.format("%.2f", bmi)))
+        Text(text = stringResource(R.string.result, "%.2f".format(bmi)))
 
     }
 }
